b246b46fb12ba60dc2b22d4c6910a2847ca1132f
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-05-04 15:32:34 +0700

    menambahkan mhs1

--- staticmember.cpp
+++ staticmember.cpp
@@ -27,4 +27,18 @@ void Mahasiswa::printAll(){
     cout << "ID : " << id << endl;
     cout << "Nama : " << nama << endl;
     cout << endl;
+};
+
+int main(){
+    
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs1.printAll();
+    mhs1.printAll();
+    mhs1.printAll();
+
 }
